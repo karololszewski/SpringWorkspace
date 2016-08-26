4d512e6586ec68eef360b9b49fd8a942be879df5
karololszewski@poczta.fm <karololszewski@poczta.fm>
2016-08-26 09:54:53 +0200

    dodanie wyszukiwarki wraz z blokada smieci

--- masterSpringMvc/src/main/java/masterSpringMvc/controller/TweetController.java
+++ masterSpringMvc/src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,6 +2,7 @@ package masterSpringMvc.controller;
 
 import java.util.List;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,13 +10,33 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
 	@Autowired
 	private Twitter twitter;
+	
 	@RequestMapping("/")
+	public String home() {
+		return "searchPage";
+	}
+	
+	
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		if (search.toLowerCase().contains("œmieci")) {
+			redirectAttributes.addFlashAttribute("error", "Spróbuj wpisaæ Spring!");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
+	}
+	
+	@RequestMapping("/result")
 	public String hello(@RequestParam(defaultValue = "SpringMvc4") String search, Model model) {
 //		SearchResults searchResults = twitter.searchOperations().search(search);
 //		String text = searchResults.getTweets().get(0).getText();
